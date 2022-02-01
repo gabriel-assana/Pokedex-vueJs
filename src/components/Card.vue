2afa3e3b9f71fc3112b8d2855865969f8f984d4f
Gabriel Assana <gabrielhhz2@gmail.com>
2022-01-31 23:06:14 -0300

    continuação da implementação do consumo de API com Axios

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="pokemon-card">
-        <p>{{pokemon.name}}</p>
-        <p>{{pokemon.type}}</p>
-        <p>{{pokemon.attack}}</p>
-        <p>{{pokemon.defender}}</p>
-        <p>{{getPokemon()}}</p>
+    <div class="pokemon-card" v-for=" (pokemon, index) in pokemons" :key="index">
+        <ul class="list-group">
+            <img class="img-pokemon" :src="urlImages" :alt="pokemon.name"
+            >
+            <li class="list-group-item">
+                {{pokemon.name}} - {{index + 1}}
+            </li>
+        </ul>
     </div>
     
 </template>
@@ -17,23 +19,34 @@ export default {
  name: "Card",
  data(){
    return{
-       url:"https://pokeapi.co/api/v2/pokemon?limit=151%27"
-       ,
-       pokemon:{
-         name: 'Pikachu',
-         type: 'Eletric',
-         attack: 50,
-         defender: 50,
-       }
-   }
+       url: "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0",
+       urlImages: [],
+       pokemons:[],
+  }
  },
  methods:{
     getPokemon() {
-        axios.get(this.url).then(response => {
-            console.log(response)
-        }).catch(error => console.log(error))
-    }
+        axios.get(this.url)
+            .then(({data}) => this.pokemons = data.results)
+            .catch(error => console.log(error))
+      },
+    getImgPokemon(){
+        for (let index = 1; index <= 150; index++) {
+                
+            axios.get(`https://pokeapi.co/api/v2/pokemon/${index}`)
+                .then(resp =>{
+                    this.urlImages.push(resp.data.sprites.front_default)
+                    console.log(this.urlImages)
+                })
+                .catch(error => console.log(error))
 
+
+        }
+    }
+ },
+ mounted(){
+    this.getPokemon(),
+    this.getImgPokemon()
  }
 }
 </script>
